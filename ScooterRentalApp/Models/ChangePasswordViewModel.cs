1cb93950b6e4cd8df446e5ab45ed6cd593300a78
LukasKrze <>
2025-04-17 00:41:37 +0200

    Hulajnoga-16 Początkowy commit

---
+++ ScooterRentalApp/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScooterRentalApp.Models
+{
+    public class SetPasswordViewModel
+    {
+        public int PatientId { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string NewPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        [Compare("NewPassword", ErrorMessage = "Wprowadzone hasła nie są takie same")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ ScooterRentalApp/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ScooterRentalApp.Models
+{
+    public class LoginViewModel
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
