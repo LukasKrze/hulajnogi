e73b47eb237c85b03a5c0806681cda21f845d357
LukasKrze <205630747+LukasKrze@users.noreply.github.com>
2025-05-18 22:06:02 +0200

    HULAJNOGA-51: Integracja kluczowych modułów – połączenie całości systemu

---
+++ ScooterRentalApp/Data/SupportMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScooterRentalApp.Data
+{
+    public class SupportMessage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Wiadomość")]
+        public string Message { get; set; } = string.Empty;
+
+        [Display(Name = "Klient")]
+        public Client Client { get; set; }
+
+        [Display(Name = "Czy wysłana przez klienta?")]
+
+        public bool FromClient { get; set; }
+
+        [Display(Name = "Data utworzenia")]
+        public DateTime Created { get; set; }
+
+    }
+}
